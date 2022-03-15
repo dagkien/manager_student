d70c77168ba94c7829f17e24ead746eb336b73c6
dag kien <kiendang712002@gmail.com>
2022-03-15 10:43:22 +0700

    setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("ANH EM NHÓM 5 MÃI ĐỈNH")
-});
 
 module.exports = router;
