f6befb75ee58e13a0e21d0b0d51961f56c15d2a7
dag kien <kiendang712002@gmail.com>
2022-03-15 11:14:49 +0700

    add full file js and stylesheet form project skydash

--- env.js
+++ env.js
@@ -3,7 +3,7 @@ module.exports = function(app, express) {
         app.use(express.logger());
         app.use(express.static(__dirname + '/public'));
         app.set('views', __dirname + '/views');
-        app.set('view engine', 'jade'); //extension of views
+        app.set('view engine', 'ejs'); //extension of views
 
     });
 
