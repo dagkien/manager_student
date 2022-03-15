d777cf6bb0a6d03abf50a2eb9c067a23db2ae119
dag kien <kiendang712002@gmail.com>
2022-03-15 10:26:45 +0700

    setup

--- routes/comment.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const {verifyToken} = require('../Middleware')
-const comment = require('../controllers/comment')
-router.post('/api/postComment', [verifyToken, comment.postComment]);
-
-module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send("ANH EM NHÓM 5 MÃI ĐỈNH")
+});
+
+module.exports = router;
