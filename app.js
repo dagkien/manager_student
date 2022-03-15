5dbb3dd7173e087c0a2a3886ef5bdaba1d930e7c
dag kien <kiendang712002@gmail.com>
2022-03-15 11:19:49 +0700

    setup

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',indexRouter);
-app.get('/', function(req,res,next){
-    res.send("Anh em nhóm 5 mãi đỉnh")
-})
+
 
 module.exports = app;
